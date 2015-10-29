b850e81c8aa88a43f40e503fc0133c92a9dfd7ee
JerryLi <jerryli.cs@gmail.com>
2015-10-29 19:53:01 +0800

    modified bin/*

--- bin/main.sh
+++ bin/main.sh
@@ -6,7 +6,10 @@ cd `dirname $0`
 logpath="/var/cache/bind/query.log"
 resultpath="../result"
 time=$(date "+%Y%m%d" -d "-1day")
-
+if [ ! -d $resultpath ]
+then
+	mkdir $resultpath
+fi
 if [ ! -d ../log_data ]
 then
 	mkdir ../log_data
@@ -27,11 +30,17 @@ then
 	mkdir ../log_data/done
 fi
 mv ../log_data/query.log ../log_data/done/query.log.$time
-#调用蛮力扫描验证递归解析器IP地址是否开放,开放的ip地址列表存入$resultpath/$time中的iplist文件中
+
+#调用dig测量脚本测量递归解析器IP地址是否开放,开放的ip地址列表存入$resultpath/$time中的ip文件中,每行格式为'ip status'
 echo "Measuring Open resolvers"
-sh ../libexec/measureopenresolver.sh ../log_result/$time/responseip $time $resultpath
+outpath=$resultpath/$time
+if [ ! -d "$outpath" ]; then
+	mkdir "$outpath"
+fi
+python ../libexec/dig_measure.py ../log_result/$time/responseip $outpath
+
 #更新数据库
 echo "Updating databases"
-sh ../libexec/createsql.sh
-python ../libexec/updatesql.py $resultpath/$time/iplist $time main
+sh ../libexec/sql_create.sh
+python ../libexec/sql_update.py $outpath/ip $time main
 #结束
--- bin/maintain.sh
+++ bin/maintain.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 cd `dirname $0`
-tmppath="/tmp/iplist"
+tmpfile="/tmp/iplist"
 time=$(date "+%Y%m%d")
 resultpath="/tmp"
 #extractor IP address from SQL database
 echo "Extractor IP address from SQL database"
-python ../libexec/extractorsql.py $tmppath
-
-if [ ! -e $tmppath ]
+python ../libexec/sql_extractor.py $tmpfile
+if [ ! -e $tmpfile ]
 then
 	echo " IP data doesn't exists"
 	exit 1
 fi
-#Measuring IP list
+
+#调用dig测量脚本测量递归解析器IP地址是否开放,开放的ip地址列表存入$resultpath/$time中的ip文件中,每行格式为'ip status'
 echo "Measuring IP list"
-sh ../libexec/measureopenresolver.sh $tmppath $time $resultpath
-#Update SQL database
-echo "Update SQL database"
-python ../libexec/updatesql.py $resultpath/$time/iplist $time maintain
+outpath=$resultpath/$time
+if [ ! -d "$outpath" ]; then
+	mkdir "$outpath"
+fi
+python ../libexec/dig_measure.py $tmpfile $outpath
+
+#更新数据库
+echo "Updating databases"
+sh ../libexec/sql_create.sh
+python ../libexec/sql_update.py $outpath/ip $time maintain
+
 #Delete tmp files
 if [ -d $resultpath/$time ]
 then
 	rm -r $resultpath/$time
 fi
-if [ -e $tmppath ]
+if [ -e $tmpfile ]
 then
-	rm $tmppath
+	rm $tmpfile
 fi
